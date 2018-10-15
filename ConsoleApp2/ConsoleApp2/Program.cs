d128a6ed51b5af7301d4fc72b70d575502fc6706
Unai Aldekozea <unaizeni@hotmail.com>
2018-10-15 18:26:17 +0200

    Ejercicio 4 (Peso)

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleApp2
             bool orbita;
             Satelite s1 = new Satelite(100, 1000, 10000);
             s1.PrintPosicion();
-            s1.SetPosicion(10, 20, 30);
+            s1.SetPosicion(10, 20, -1);
             s1.PrintPosicion();
             s1.EnOrbita();
             orbita = s1.EnOrbita();
--- Ejercicio4/Ejercicio4/Program.cs
+++ Ejercicio4/Ejercicio4/Program.cs
@@ -10,6 +10,13 @@ namespace Ejercicio4
     {
         static void Main(string[] args)
         {
+            int peso = 5;
+            String tipo = "LB";
+            Peso p1 = new Peso(peso, tipo);
+            //Console.WriteLine(p1.GetPeso(p1.GetKg(), "lb"));
+            Console.WriteLine(peso + " " + tipo + " = " + p1.GetKg() + "kg");
+            Console.WriteLine(p1.GetKg() + "kg =" + p1.GetPeso("lb") + " libras");
+            Console.ReadLine();
         }
     }
 }
