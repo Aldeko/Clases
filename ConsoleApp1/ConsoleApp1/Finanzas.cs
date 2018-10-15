ce1aff8ea72e309fad42b7c09cf717119c73a5c9
Unai Aldekozea <unaizeni@hotmail.com>
2018-10-15 13:24:18 +0200

    Ejercicio 1 OPP

---
+++ ConsoleApp1/ConsoleApp1/Finanzas.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Finanzas
+    {
+        private double cambio;
+
+        //CONSTRUCTOR POR DEFECTO
+        public Finanzas()
+        {
+            this.cambio = 0.92;
+
+        }
+
+        public Finanzas(double cambio)
+        {
+            this.cambio = cambio;
+        }
+
+        public double GetCambio()
+        {
+
+            return cambio;
+
+        }
+        public void SetCambio(double cambio)
+        {
+
+            this.cambio = cambio;
+
+        }
+            //METODOS
+        public double DolaresToEuros(double cantidad)
+        {
+        return cantidad * cambio;
+        }
+        public double EurosToDolares(double cantidad)
+        {
+            return cantidad / cambio;
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Finanzas f1 = new Finanzas(0.99);
+            f1.SetCambio(0.92);
+            
+            Console.WriteLine("5 euros son: " + f1.EurosToDolares(40) + "  dolares");
+            Console.WriteLine("5 dolares son: " + f1.DolaresToEuros(50) + "dolares");
+            Console.ReadLine();
+
+           
+        }
+    }
+}
