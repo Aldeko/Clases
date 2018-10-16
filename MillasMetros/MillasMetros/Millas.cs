401e23ee6e03e7c624eff6486b99a44b8ae1f8ff
Unai Aldekozea <unaizeni@hotmail.com>
2018-10-16 09:12:40 +0200

    Ejercicio Millas a metros

---
+++ MillasMetros/MillasMetros/Millas.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MillasMetros
+{
+    class Millas
+    {
+        private double millas;
+        private double metros;
+
+        public Millas()
+        {
+
+        }
+        public Millas(double millas)
+        {
+            this.millas = millas;
+            
+        }
+
+        public double Metros(double millas)
+        {
+            return millas * 1852;
+        }
+        public double Kilometros(double metros)
+        {
+            return metros / 1000;
+        }
+        
+    }
+}
---
+++ MillasMetros/MillasMetros/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MillasMetros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double millas = 25;
+            double metros;
+            Millas m1 = new Millas(millas);
+            Console.WriteLine(millas + " millas son" + m1.Metros(millas) + " metros");
+            Console.WriteLine(m1.Metros(millas) + " metros son" + m1.Kilometros(m1.Metros(millas)) + " Kilometros");
+
+            Console.ReadLine();
+           
+
+        }
+    }
+}
