0fd64fc09f3cd34c8f8a3254d8c91daa11d0ec6e
Unai Aldekozea <unaizeni@hotmail.com>
2018-10-16 10:14:26 +0200

    Ejercicio 6

---
+++ Ejercicio 6/Ejercicio 6/Coches.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_6
+{
+    class Coches
+    {
+
+        private string marca;
+        private string modelo;
+        private string clase;
+
+        public Coches()
+        {
+            this.marca = "Audi";
+            this.modelo = "A3";
+            this.clase = "Sportback";
+
+        }
+
+        public Coches (string marca, string modelo, string clase)
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.clase = clase;
+        }
+
+        //GETTERS y SETTERS
+
+        public string GetMarca()
+        {
+            return marca;
+        }
+        public void SetMarca(string marca)
+        {
+            this.marca = marca;
+        }
+        public string GetModelo()
+        {
+            return modelo;
+        }
+        public void SetModelo(string modelo)
+        {
+            this.modelo = modelo;
+        }
+        public string GetClase()
+        {
+            return clase;
+        }
+        public void SetClase(string clase)
+        {
+            this.clase = clase;
+        }
+
+        //METODOS
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Marca " + marca + " modelo " + modelo + " Clase " + clase);
+        }
+
+
+    }
+}
---
+++ Ejercicio 6/Ejercicio 6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Coches c1 = new Coches();
+            c1.PrintInfo();
+            c1.SetMarca("Lamborghini");
+            c1.SetModelo("Murcielago");
+            c1.SetClase("Fucker");
+            Console.WriteLine(" ");
+            c1.PrintInfo();
+            Console.ReadLine();
+            
+        }
+    }
+}
